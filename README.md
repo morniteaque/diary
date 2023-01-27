da2c22fbc1e67e9fa80941d75b3d0b1fff55bf14
Felicitas Pojtinger <felicitas@pojtinger.com>
2023-01-27 23:54:03 +0100

    feat: Add basic NextJS boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # diary
+
 An extensive transition diary site.
+
+## Overview
+
+🚧 This project is a work-in-progress! Instructions will be added as soon as it is usable. 🚧
+
+## License
+
+diary (c) 2023 Emmeryn Harper, Felicitas Pojtinger and contributors
+
+SPDX-License-Identifier: AGPL-3.0
---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Diary</title>
+        <meta name="description" content="An extensive transition diary" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <h1>Diary</h1>
+    </>
+  )
+}
