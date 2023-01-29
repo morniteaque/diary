165de1209964505ad6de390d3e3050c79723a08f
Felicitas Pojtinger <felicitas@pojtinger.com>
2023-01-29 23:58:30 +0100

    feat: Start implementation of settings and help tabs

--- components/home.tsx
+++ components/home.tsx
@@ -2,6 +2,7 @@ import {
   Brand,
   Button,
   ButtonVariant,
+  Divider,
   Nav,
   NavItem,
   NavList,
@@ -13,11 +14,21 @@ import {
   PageSection,
   PageSectionVariants,
   SkipToContent,
+  Switch,
   Text,
   TextContent,
+  Toolbar,
+  ToolbarContent,
+  ToolbarItem,
 } from "@patternfly/react-core";
-import { CogIcon, DollarSignIcon } from "@patternfly/react-icons";
+import {
+  CogIcon,
+  ExternalLinkAltIcon,
+  FilterIcon,
+  HelpIcon,
+} from "@patternfly/react-icons";
 import Link from "next/link";
+import { useState } from "react";
 import icon from "../docs/icon-dark.png";
 
 const NAV_ITEMS = [
@@ -33,6 +44,10 @@ const NAV_ITEMS = [
 ];
 
 export default function Home() {
+  const [settingsOpen, setSettingsOpen] = useState(false);
+  const [helpOpen, setHelpOpen] = useState(false);
+  const [nsfw, setNSFW] = useState(false);
+
   return (
     <Page
       header={
@@ -42,18 +57,53 @@ export default function Home() {
             <PageHeaderTools>
               <PageHeaderToolsGroup>
                 <PageHeaderToolsItem>
-                  <Button aria-label="Settings" variant={ButtonVariant.plain}>
+                  <Button
+                    aria-label="Settings"
+                    onClick={() => {
+                      setHelpOpen(false);
+                      setSettingsOpen((v) => !v);
+                    }}
+                    isActive={settingsOpen}
+                    variant={
+                      settingsOpen ? ButtonVariant.control : ButtonVariant.plain
+                    }
+                  >
                     <CogIcon />
                   </Button>
                 </PageHeaderToolsItem>
+
                 <PageHeaderToolsItem>
                   <Button
-                    icon={<DollarSignIcon />}
+                    aria-label="Help"
+                    onClick={() => {
+                      setSettingsOpen(false);
+                      setHelpOpen((v) => !v);
+                    }}
+                    isActive={helpOpen}
+                    variant={
+                      helpOpen ? ButtonVariant.control : ButtonVariant.plain
+                    }
+                  >
+                    <HelpIcon />
+                  </Button>
+                </PageHeaderToolsItem>
+
+                <Divider
+                  inset={{ default: "insetMd" }}
+                  orientation={{
+                    default: "vertical",
+                  }}
+                  className="pf-x-u-opacity-muted pf-u-mx-sm"
+                />
+
+                <PageHeaderToolsItem>
+                  <Button
                     aria-label="Donate"
                     variant={ButtonVariant.link}
                     className="pf-x-u-color--unset"
                   >
                     Donate
+                    <ExternalLinkAltIcon className="pf-u-ml-sm" />
                   </Button>
                 </PageHeaderToolsItem>
               </PageHeaderToolsGroup>
@@ -83,8 +133,43 @@ export default function Home() {
         <SkipToContent href="#main">Skip to content</SkipToContent>
       }
       mainContainerId="main"
+      additionalGroupedContent={
+        (settingsOpen || helpOpen) && (
+          <PageSection variant={PageSectionVariants.light} isWidthLimited>
+            {settingsOpen ? (
+              <Toolbar>
+                <ToolbarContent>
+                  <ToolbarItem>
+                    <Switch
+                      label="NSFW"
+                      labelOff="NSFW"
+                      isChecked={nsfw}
+                      onChange={setNSFW}
+                      isReversed
+                    />
+                  </ToolbarItem>
+
+                  <ToolbarItem variant="separator" />
+
+                  <ToolbarItem>Slider</ToolbarItem>
+
+                  <ToolbarItem variant="separator" />
+
+                  <ToolbarItem>
+                    <Button aria-label="Filters" variant={ButtonVariant.plain}>
+                      <FilterIcon />
+                    </Button>
+                  </ToolbarItem>
+                </ToolbarContent>
+              </Toolbar>
+            ) : (
+              "Help"
+            )}
+          </PageSection>
+        )
+      }
     >
-      <PageSection variant={PageSectionVariants.light}>
+      <PageSection>
         <TextContent>
           <Text component="h1">Body</Text>
         </TextContent>
