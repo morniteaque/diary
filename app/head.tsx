f72d0841db2c70467ca13b0391597adb56dba162
Felicitas Pojtinger <felicitas@pojtinger.com>
2023-02-18 23:54:19 +0100

    feat: Add favicon

--- app/head.tsx
+++ app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>Diary</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="An extensive transition diary" />
-      <link rel="icon" href="data:," />
+      <link rel="icon" href="/icon-light.png" />
     </>
   );
 }
